2f40c1deb5683fa08adf8545f29a00c6e14656dc
keyready <vip.korchak@list.ru>
2024-03-14 12:31:33 +0300

    уменьшил шрифт истории

--- client/src/entities/HistoryItem/ui/HistoryItem/HistoryItem.tsx
+++ client/src/entities/HistoryItem/ui/HistoryItem/HistoryItem.tsx
@@ -21,7 +21,6 @@ export const HistoryItem = memo((props: HistoryItemProps) => {
             <Text
                 className={classes.titleWrapper}
                 title={item.title}
-                size="small"
                 titleClassname={classes.title}
             />
             <AppLink target="_blank" to={item.url} className={classes.link}>
